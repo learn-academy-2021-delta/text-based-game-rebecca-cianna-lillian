335983bec2a844fe0c97c5ff0d63539e3bfa0ec0
LiangchengR <liangchengr@gmail.com>
2021-10-05 14:33:21 -0700

    coded some beginning instructions and comments

---
+++ text-based-rebecca-cianna-lillian.rb
@@ -0,0 +1,11 @@
+p 'TV/Movie Personality Test'
+#insert customized greeting
+p 'Respond to the following questions by typing the number that corresponds to your answer.'
+
+p 'Question 1: If extra lives were a thing in the real world, how would you get them?'
+p '1)'
+p ''
+p
+p
+
+#get.chomp - getting user inputs
