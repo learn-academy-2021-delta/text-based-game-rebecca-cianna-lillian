ec7f96c5974e95ffd7f11f63bac27c768337f1a3
LiangchengR <liangchengr@gmail.com>
2021-10-05 16:14:26 -0700

    MVP of text based game

--- text-based-rebecca-cianna-lillian.rb
+++ text-based-rebecca-cianna-lillian.rb
@@ -1,6 +1,9 @@
-p 'TV/Movie Personality Test'
+puts ' '
+puts 'TV/Movie Personality Test'
 #insert customized greeting
 puts 'Respond to the following questions by typing the number that corresponds to your answer.'
+puts ' '
+puts ' '
 
 bella = 0
 sponge = 0
@@ -17,7 +20,8 @@ puts "4) I'd steal them from people...duh."
 puts "5) Lets arm wrestle for them!"
 
 response << gets.chomp.to_i
-p response
+puts ' '
+puts ' '
 
 puts 'Question 2: What cocktail best describes you?'
 puts "1) Beer on tap."
@@ -27,7 +31,8 @@ puts "4) A shot of Tequila."
 puts "5) Old Fashioned."
 
 response << gets.chomp.to_i
-p response
+puts ' '
+puts ' '
 
 puts 'Question 3: What dog best describes your personality?'
 puts "1) Pug."
@@ -37,7 +42,8 @@ puts "4) Pit Bull."
 puts "5) German Shephard."
 
 response << gets.chomp.to_i
-p response
+puts ' '
+puts ' '
 
 puts "Question 4: Someone bought you a drink from across the bar, what's your reaction?"
 puts "1) Someone bought me a drink? Must be the wrong person."
@@ -47,7 +53,8 @@ puts "4) What do you want from me (...seems suspicious)?"
 puts "5) *Looks over and winks*"
 
 response << gets.chomp.to_i
-p response
+puts ' '
+puts ' '
 
 puts 'Question 5: If you were a Disney princess, which one would you be?'
 puts "1) Belle."
@@ -57,7 +64,8 @@ puts "4) Merida."
 puts "5) Mulan."
 
 response << gets.chomp.to_i
-p response
+puts ' '
+puts ' '
 
 puts "Question 6: You're on your way to work stuck in traffic. Someone just cut you off and you just rear ended them. How do you react?"
 puts "1) Cry."
@@ -67,10 +75,58 @@ puts "4) Let's fight!"
 puts "5) Honking and yelling!"
 
 response << gets.chomp.to_i
-p response
+puts ' '
+puts ' '
 
-characters = ['Bella Swan', 'Spongebob Squarepants', 'Regina George', 'Harley Quinn', 'Maverick']
-[2, 3, 2, 5, 3, 2]
+# characters = ['Bella Swan', 'Spongebob Squarepants', 'Regina George', 'Harley Quinn', 'Maverick']
+# [2, 3, 2, 5, 3, 2]
+
+#iterates through the array of responses and increases count of the character based on responses
+response.each do |value|
+    case value
+    when 1
+        bella += 1
+    when 2
+        sponge += 1
+    when 3
+        regina += 1
+    when 4
+        harley += 1
+    when 5
+        maverick += 1
+    else
+        puts 'Something went wrong, do the quiz again.'
+    end
+end
+
+#pushes all the counts to an array
+characters =[]
+characters << bella
+characters << sponge
+characters << regina
+characters << harley
+characters << maverick
+
+#finds the index of the max count in that array
+personality = characters.index(characters.max) 
+
+#based on the index, return which character you are
+case personality
+when 0
+    puts "You're Bella Swan!"
+when 1
+    puts "You're Spongebob Squarepants!"
+when 2
+    puts "You're Regina George!"
+when 3
+    puts "You're Harley Quinn!"
+when 4
+    puts "You're Maverick!"
+else
+    puts 'Something went wrong, do the quiz again.'
+end
+
+
 
 
 
